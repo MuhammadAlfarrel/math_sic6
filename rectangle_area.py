f3cead0d02d796179d541cc78198a98ae06442c5
Muhammad Alfarrel Arya Mahardika <alfarrelmahardika@gmail.com>
2025-02-08 11:18:39 +0700

    Memperbaiki rumus luas segitiga dan persegi panjang

--- rectangle_area.py
+++ rectangle_area.py
@@ -1,8 +1,13 @@
-def calculate_area(length, width):
-    return length * width * 0.5 
+def calculate_triangle_area(length, height):
+    return length * height* 0.5 
 
+def calculate_rectangle_area(length, width):
+    return length * width
 # Example usage (for testing)
 length = 5
 width = 10
-area = calculate_area(length, width)
-print(f"The area of the rectangle is: {area}")
+height = 20
+area_rectangle = calculate_rectangle_area(length, width)
+area_triangle = calculate_triangle_area(length, height)
+
+print(f"The area of the rectangle is: {area_rectangle}")
